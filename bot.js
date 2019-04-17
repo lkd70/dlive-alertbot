2a60731a6d1dc567255bd349c1178c02e2db684b
LKD70 <lkdornan70@gmail.com>
2019-04-17 22:37:53 +0100

    Added streamer/stream info to discord webhook

--- bot.js
+++ bot.js
@@ -10,16 +10,22 @@ config.channels.forEach(channel => bots.push({ dlive: new dlive(channel.id, conf
 
 bots.forEach(bot => {
 	bot.dlive.on('ChatLive', () => {
-		axios.post(bot.channel.webhook, {
-			content: bot.channel.tag,
-			embeds: [ {
-				title: bot.channel.name,
-				type: 'rich',
-				description: `${bot.channel.name} is going live!`,
-				url: `https://dlive.tv/${bot.channel.name}`
-			}
-			]
+		bot.dlive.getChannelInformation(bot.channel.name, res => {
+			axios.post(bot.channel.webhook, {
+				content: bot.channel.tag,
+				embeds: [ {
+					title: `${bot.channel.name} - ${res.livestream.category.title}`,
+					type: 'rich',
+					description: `${bot.channel.name} is going live!`,
+					url: `https://dlive.tv/${bot.channel.name}`,
+					thumbnail: { url: res.avatar },
+					image: { url: res.livestream.category.imgUrl },
+					fields: [
+						{ name: 'Category', value: res.livestream.category.title }
+					]
+				}
+				]
+			});
 		});
 	});
-
 });
